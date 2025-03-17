d0e93b18f6cb5a46a6089403529041cf6d82252c
dev_hiago <minimessijsilva@gmail.com>
2025-03-17 13:38:16 -0300

    feat(task-0): creation header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <h1 class="header-title">Instituto JavaScript</h1>
+    <router-link to="/">
+      <img class="header-logo" src="@/assets/logo.jpg" size="65" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+@import "./header.styles.scss";
+</style>
