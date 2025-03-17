f425fa06376a81d0a069595f3dd3b2ae871ba438
dev_hiago <minimessijsilva@gmail.com>
2025-03-17 13:37:52 -0300

    feat(task-0): creation of the academic module component

---
+++ src/components/AcademicModuleHeaderContent/AcademicModuleHeaderContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="academic-module-header-content">
+    <div class="title">Academic Module</div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AcademicModuleHeaderContent",
+};
+</script>
+
+<style scoped>
+@import "./academicModuleHeaderContent.styles.scss";
+</style>
