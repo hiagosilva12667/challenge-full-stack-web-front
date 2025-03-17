776d438f6ae354c87b2d3150df9828cea77add8c
dev_hiago <minimessijsilva@gmail.com>
2025-03-17 13:27:30 -0300

    feat(task-0): creating configuration files

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,26 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Composables
+import { createVuetify } from "vuetify";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      light: {
+        colors: {
+          background: "#f5f5f5", // Define o fundo padrão
+        },
+      },
+    },
+  },
+});
