5812ed9a034389ff7c6757dfbcf208a1d44833cf
dev_hiago <minimessijsilva@gmail.com>
2025-03-17 13:39:11 -0300

    feat(task-0): creation student management header component

---
+++ src/components/StudentManagementHeader/StudentManagementHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="student-management-header">
+    <h1 class="student-management-header--title">{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import type { IStudentManagementHeader } from "./models/studentManagementHeader.interface";
+import type { PropType } from "vue";
+export default {
+  name: "StudentManagementHeader",
+  props: {
+    title: {
+      type: String as PropType<IStudentManagementHeader["title"]>,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "./studentManagementHeader.styles.scss";
+</style>
