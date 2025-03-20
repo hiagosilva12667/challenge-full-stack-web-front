59c27f8925e94e38bbf004262b6e0cc96e640dd7
dev_hiago <minimessijsilva@gmail.com>
2025-03-20 12:25:00 -0300

    feat(task-1): create logout component

---
+++ src/components/LogoutButton/LogoutButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn color="red" @click="logout">Sair</v-btn>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import AuthService from "@/services/authService";
+
+const router = useRouter();
+
+const logout = () => {
+  AuthService.logout();
+  router.push("/login");
+};
+</script>
