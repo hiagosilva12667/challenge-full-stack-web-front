200b04e1f68fe8861f1d0e9ec0b376822ec87a37
dev_hiago <minimessijsilva@gmail.com>
2025-03-20 17:23:29 -0300

    fix(task-4): importing the create account file from views

--- src/pages/create-account/index.vue
+++ src/pages/create-account/index.vue
@@ -1,47 +1,9 @@
 <template>
-  <v-container>
-    <v-form @submit.prevent="register">
-      <v-text-field v-model="formData.name" placeholder="Nome" required />
-      <v-text-field v-model="formData.email" placeholder="Email" required />
-      <v-text-field
-        v-model="formData.phone_number"
-        placeholder="Telefone"
-        required
-      />
-      <v-text-field v-model="formData.cpf" placeholder="CPF" required />
-      <v-text-field
-        v-model="formData.password"
-        type="password"
-        placeholder="Senha"
-        required
-      />
-      <v-btn color="blue" type="submit">Criar Conta</v-btn>
-
-      <!-- Exibir erro caso exista -->
-      <p v-if="authStore.errorMessage" class="error">
-        {{ authStore.errorMessage }}
-      </p>
-    </v-form>
-  </v-container>
+  <CreateAccount />
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { useAuthStore } from "@/stores/auth";
-
-const authStore = useAuthStore();
-
-const formData = ref({
-  name: "",
-  email: "",
-  phone_number: "",
-  cpf: "",
-  password: "",
-});
-
-const register = async () => {
-  await authStore.createAccount(formData.value);
-};
+import CreateAccount from "@/views/createAccount/createAccount.vue";
 </script>
 
 <style lang="scss"></style>
